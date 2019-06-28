d7130148da0d40bc289394ab8c6800cc1b25df51
ISPLab <for_orlov@mail.ru>
2019-06-28 19:01:41 +0300

    GTKStart

---
+++ GtkSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+namespace GtkSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            Window win = new Window("BSDWindow");
+            win.Resize(200, 200);
+            Label label = new Label();
+            label.Text = "For Frebsd";
+            win.Add(label);
+            win.ShowAll();
+            Application.Run();
+        }
+    }
+}
---
+++ GtkStart/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Gtk;
+
+namespace GtkStart
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			try
+			{ 
+			Application.Init();
+			Window win = new Window("GtkStart FreeBSD window");
+			win.Resize(200, 200);
+				Gtk.Button but = new Button(); 
+			Label myLabel = new Label();
+			myLabel.Text = "Gtk label";
+			//Add the label to the form
+		 	win.Add(myLabel);
+			win.ShowAll();
+			Application.Run();
+	  	    }
+			catch(Exception ex)
+			{
+
+			}
+		}
+	}
+}
+
