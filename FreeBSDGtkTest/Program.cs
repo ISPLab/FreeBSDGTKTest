313b524cf7be59cea0d636cb6488ab1813f17113
ISPLab <for_orlov@mail.ru>
2019-06-28 17:40:17 +0300

    adding gtk

---
+++ FreeBSDGtkTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gtk;
+namespace FreeBSDGtkTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Application.Init();
+                Window win = new Window("GtkStart FreeBSD window");
+                win.Resize(200, 200);
+              //  Gtk.Button but = new Button();
+                Label myLabel = new Label();
+                myLabel.Text = "Gtk label special for freebsd";
+                //Add the label to the form
+                win.Add(myLabel);
+                win.ShowAll();
+                Application.Run();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
