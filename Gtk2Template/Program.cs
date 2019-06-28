be3dcf846fb6b2e574648778580f85243c2c09b1
ISPLab <for_orlov@mail.ru>
2019-06-28 18:39:18 +0300

    gtk template

---
+++ Gtk2Template/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+
+namespace Gtk2Template
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            Label label = new Label();
+            label.Text = "For Frebsd";
+            win.Add(label);
+            win.ShowAll();
+            Application.Run();
+        }
+    }
+}
